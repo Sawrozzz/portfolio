7db59e7578e6963f63114daea0ce20020dd9ecc8
[saroj adhikari] <sawroz.adhikari@gmail.com>
2024-02-19 09:29:53 +0545

    added new features

--- contact.js
+++ contact.js
@@ -13,3 +13,16 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+function sendMail() {
+  var params = {
+    from_name: document.getElementById("name").value,
+    email_id: document.getElementById("email_id").value,
+    message: document.getElementById("message").value,
+  };
+  emailjs
+    .send("service_1xarxli", "template_e78y81v", params)
+    .then(function (res) {
+      alert("Success!" + res.status);
+    });
+}
